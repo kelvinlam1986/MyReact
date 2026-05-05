15d2d4167f79909af7341283784db397ae24d3b0
kelvinlam <kelvincoder@gmail.com>
2026-05-05 22:14:08 +0700

    Add Validation Behavior middleware

---
+++ Application/Core/ValidationBehavior.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace Application.Core
+{
+    public class ValidationBehavior<TRequest, TResponse>(IValidator<TRequest> validator = null)
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (validator == null)
+            {
+                return await next();
+            }
+
+            var validateResult = await validator.ValidateAsync(request, cancellationToken);
+            if (!validateResult.IsValid)
+            {
+                throw new ValidationException(validateResult.Errors);
+            }
+
+            return await next();
+        }
+    }
+}
